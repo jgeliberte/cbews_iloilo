b02d60ddc2de58ea3052115809ad732de0a0f2ec
jlnepomuceno <nepomuceno.jo.lo@gmail.com>
2020-02-17 11:33:32 +0800

    Updates on login, signup, and CRA upload download

--- src/api/risk_assessment/cra.py
+++ src/api/risk_assessment/cra.py
@@ -1,9 +1,10 @@
-from flask import Blueprint, jsonify, request, redirect, url_for, send_from_directory
+from flask import Blueprint, jsonify, request, redirect, url_for, send_from_directory, send_file
 from connections import SOCKETIO
 from pprint import pprint
 import time
 import os
 from src.model.community_risk_assessment import CommunityRiskAssessment
+from config import APP_CONFIG
 
 COMMUNITY_RISK_ASSESSMENT_BLUEPRINT = Blueprint("community_risk_assessment", __name__)
 
@@ -15,15 +16,69 @@ def fetch():
     files = []
 
     for file in cra_list:
+        file_type = file.split(".")[1]
         files.append({
             "filename": file,
-            "file_type": file.split(".")[1],
+            "file_type": file_type,
             "file_path": data['path']
         })
+    # return {"status": True, "data": files}
     return jsonify({"status": True, "data": files})
+
 
 @COMMUNITY_RISK_ASSESSMENT_BLUEPRINT.route("/cra/community_risk_assessment/upload", methods=["POST"])
 def upload():
-    file = request.files['resource']
-    file.save(os.path.join('/var/www/html/CBEWSL/MARIRONG/DOCUMENTS/', file.filename))
-    return jsonify({"status": True})
+    try:
+        file = request.files['file']
+        directory = f"{APP_CONFIG['MARIRONG_DIR']}/DOCUMENTS/"
+        filename = file.filename
+
+        # if os.path.exists(f"{directory}{filename}"):
+        #     count = filename.count(".")
+        #     name_list = filename.split(".", count)
+        #     file_type = f".{name_list[count]}"
+        #     name_list.pop()
+        #     filename = f"{'.'.join(name_list)}-copy{file_type}"
+        # file.save(os.path.join(directory, filename))
+
+
+        count = filename.count(".")
+        name_list = filename.split(".", count)
+        file_type = f".{name_list[count]}"
+        name_list.pop()
+        filename = f"{'.'.join(name_list)}"
+
+        temp = f"{filename}{file_type}"
+        uniq = 1
+        while os.path.exists(f"{directory}{temp}"):
+            temp = '%s_%d%s' % (filename, uniq, file_type)
+            uniq += 1
+
+        file.save(os.path.join(directory, temp))
+
+        return_data = { "status": True }
+    except Exception as err:
+        raise err
+        # return_data = { "status": False }
+
+    return jsonify(return_data)
+
+
+# @COMMUNITY_RISK_ASSESSMENT_BLUEPRINT.route("/cra/community_risk_assessment/download", methods=["POST"])
+# # @COMMUNITY_RISK_ASSESSMENT_BLUEPRINT.route("/cra/community_risk_assessment/download/<filename>", methods=["POST"])
+# def download():
+#     print("==================================")
+#     data = request.get_json()
+#     print(data)
+#     filename = data["filename"]
+#     print("FILENAME", filename)
+#     file_type = filename.split(".")[1]
+#     mime_type = f"application/{file_type}"
+#     print("======")
+#     print(file_type)
+#     try:
+#         return send_from_directory(data["file_path"], filename, as_attachment=True, mimetype=mime_type, attachment_filename=(str(filename) + f".{file_type}"))
+        
+#     except Exception as e:
+#         print(e)
+#         return jsonify({ "status": False})
