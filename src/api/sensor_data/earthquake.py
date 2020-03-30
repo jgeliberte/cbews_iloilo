a2a0e2aaeabf2f1b329c32fb0bb24613682412df
John Geliberte <gelibertejohn@gmail.com>
2020-03-30 21:19:40 +0800

    + Refactored eq tables

--- src/api/sensor_data/earthquake.py
+++ src/api/sensor_data/earthquake.py
@@ -8,9 +8,9 @@ from src.model.eq_data import Earthquake as eq
 
 EARTHQUAKE_BLUEPRINT = Blueprint("earthquake_blueprint", __name__)
 
-@EARTHQUAKE_BLUEPRINT.route("/sensor_data/earthquake/", methods=["GET"])
-def fetch():
-    eq_data = eq.get_latest_eq_events()
+@EARTHQUAKE_BLUEPRINT.route("/sensor_data/earthquake/<limit>", methods=["GET"])
+def fetch(limit):
+    eq_data = eq.get_latest_eq_events(limit)
     reconstruct_data = []
     for x in eq_data:
         reconstruct_data.append({
@@ -20,9 +20,8 @@ def fetch():
             'dept': x[3],
             'lat': x[4],
             'lon': x[5],
-            'critical_distance': x[6],
-            'issuer': x[7],
-            'processed': x[8]
+            'distance': x[6],
+            'site': x[7]
         })
     if len(reconstruct_data) != 0:
         ret_val = {
